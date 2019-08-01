b0f35450f24840336a5332e0eef753dd8808f18f
Jenn Gonzalez <jgonz9873@gmail.com>
2019-07-31 23:04:07 -0700

    set up database config, sql and seeds

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,6 @@
+INSERT INTO noteful_folders (name)
+VALUES
+    ('Important'),
+    ('Super'),
+    ('Spangley')
+;
---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,12 @@
+INSERT INTO noteful_notes (title, content, date_modified, folder_id)
+VALUES
+ ('Example Title 1', 'Example Content 1', '2019-01-16 12:00:00', 2),
+ ('Example Title 2', 'Example Content 2', '2019-02-16 12:00:00', 1),
+ ('Example Title 3', 'Example Content 3', '2019-03-16 12:00:00', 2),
+ ('Example Title 4', 'Example Content 4', '2019-04-16 12:00:00', 2),
+ ('Example Title 5', 'Example Content 5', '2019-05-16 12:00:00', 3),
+ ('Example Title 6', 'Example Content 6', '2019-06-16 12:00:00', 1),
+ ('Example Title 7', 'Example Content 7', '2019-07-16 12:00:00', 1),
+ ('Example Title 8', 'Example Content 8', '2019-08-16 12:00:00', 3),
+ ('Example Title 9', 'Example Content 9', '2019-09-16 12:00:00', 2),
+ ('Example Title 10', 'Example Content 10', '2019-10-16 12:00:00', 3);
